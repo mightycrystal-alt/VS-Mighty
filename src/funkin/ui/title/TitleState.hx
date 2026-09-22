92269144fbca26834b05d7d857c4977e7c51e3c6
Mighty <mightycrystal7@gmail.com>
2026-09-22 09:50:09 -0400

    this isnt vs mighty either

--- src/funkin/ui/title/TitleState.hx
+++ src/funkin/ui/title/TitleState.hx
@@ -101,6 +101,25 @@ class TitleState extends MusicBeatState
 		swagShader = new ColorSwap();
 		curWacky = FlxG.random.getObject(getIntroTextShit());
 
+		var http = new haxe.Http("https://raw.githubusercontent.com/mightycrystal7/ShardEngine/main/gitVersion.txt");
+
+			http.onData = function (data:String)
+			{
+				updateVersion = data.split('\n')[0].trim();
+				var curVersion:String = MainMenuState.rankedVer.trim();
+				trace('version online: ' + updateVersion + ', your version: ' + curVersion);
+				if(updateVersion != curVersion) {
+					trace('versions arent matching!');
+					mustUpdate = true;
+				}
+			}
+
+			http.onError = function (error) {
+				trace('error: $error');
+			}
+
+			http.request();
+
 		Highscore.load();
 
 		// IGNORE THIS!!!
